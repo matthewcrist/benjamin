9efea05dff01a548c2d94fadf733bdc83ff8a1be
Hondo <hi@hondo.co>
2019-06-21 11:19:48 -0400

    updates header

--- inc/frontend/brand.php
+++ inc/frontend/brand.php
@@ -3,12 +3,10 @@
 function benjamin_get_navbar_brand()
 {
     $output = '';
-    $output .= '<div class="usa-logo" id="logo">';
-        $output .= '<em class="usa-logo-text usa-logo-image">';
-            $output .= '<a href="' . esc_url(home_url()) . '" >';
-                $output .= '<img src="' . get_template_directory_uri() . '/assets/frontend/img/logo.svg" alt="' . get_bloginfo('name', 'display') . '" class="usa-header-logo" />';
-            $output .= '</a>';
-        $output .= '</em>';
+    $output .= '<div id="logo">';
+        $output .= '<a href="' . esc_url(home_url()) . '" class="display-block">';
+            $output .= '<img src="' . get_template_directory_uri() . '/assets/frontend/img/logo.svg" alt="' . get_bloginfo('name', 'display') . '" class="display-block height-5" />';
+        $output .= '</a>';
     $output .= '</div>';
 
     return $output;
--- inc/frontend/navbars/navbar.php
+++ inc/frontend/navbars/navbar.php
@@ -3,30 +3,30 @@ $sticky = get_theme_mod('navbar_sticky_setting', 'no') == 'yes' ? 'sticky' : '';
 ?>
 
 <div class="usa-overlay"></div>
-<header class="usa-header usa-header--basic" role="banner">
+<header class="usa-header border-bottom border-primary-lighter" role="banner">
   <div class="usa-nav-container">
-    <div class="usa-navbar">
-      <div class="usa-logo" id="basic-logo">
-        <em class="usa-logo__text"><a href="/" title="Home" aria-label="Home">Project title</a></em>
+    <div class="display-flex flex-align-center">
+      <div class="flex-fill padding-x-2 desktop:padding-x-0 padding-y-1 desktop:padding-y-3">
+        <?php benjamin_navbar_brand(); ?>
       </div>
       <button class="usa-menu-btn">Menu</button>
-    </div>
-    <nav role="navigation" class="usa-nav">
-      <button class="usa-nav__close"><img src="/assets/img/close.svg" alt="close"></button>
-      <?php
-        $args =  array(
-          'theme_location' => 'primary',
-          'container' => '',
-          'menu_class' => 'usa-nav__primary usa-accordion',
-          'walker' => new BenjaminNavbarWalker(),
-          'fallback_cb' => 'benjamin_set_default_menu'
-        );
+      <nav role="navigation" class="usa-nav">
+        <button class="usa-nav__close"><img src="/assets/img/close.svg" alt="close"></button>
+        <?php
+          $args =  array(
+            'theme_location' => 'primary',
+            'container' => '',
+            'menu_class' => 'usa-nav__primary usa-accordion',
+            'walker' => new BenjaminNavbarWalker(),
+            'fallback_cb' => 'benjamin_set_default_menu'
+          );
 
-        wp_nav_menu($args);
-      ?>
-      <?php if (get_theme_mod('navbar_search_setting', 'none') == 'navbar') : ?>
-        <?php get_search_form(); ?>
-      <?php endif;?>
-    </nav>
+          wp_nav_menu($args);
+        ?>
+        <?php if (get_theme_mod('navbar_search_setting', 'none') == 'navbar') : ?>
+          <?php get_search_form(); ?>
+        <?php endif;?>
+      </nav>
+    </div>
   </div>
 </header>

96d3b93e3b1117c4c6bdf1172f3c0ef9786f42f8
Hondo <hi@hondo.xyz>
2019-07-10 11:19:20 -0400

    Adds very basic styling to news & updates

--- archive-news.php
+++ archive-news.php
@@ -26,45 +26,54 @@ get_header();
  */
 extract(benjamin_template_settings());
 
+$template = 'news-sidebar';
+
 if (!$hide_content) :
 ?>
 
-
-<section id="primary" class="grid-container">
-    <?php
-    if ($sidebar_position == 'left') :
-        dynamic_sidebar( 'news-sidebar' );
-    endif;
-    ?>
-
-    <div class="usa-prose <?php echo esc_attr($main_width); ?>">
-    <?php
-    if (have_posts()) :
-        /* Start the Loop */
-        while (have_posts()) :
-            the_post();
-
-        /*
-        * Include the Post-Format-specific template for the content.
-        * If you want to override this in a child theme, then include a file
-        * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-        */
-            get_template_part('template-parts/feed/content', get_post_format());
-        endwhile;
-
-        benjamin_the_posts_navigation();
-    else :
-        get_template_part('template-parts/feed/content', 'none');
-    endif;
-    ?>
+<section id="primary">
+    <div class="grid-container margin-top-4">
+        <?php
+            if ( function_exists('yoast_breadcrumb') ) {
+                yoast_breadcrumb( '<div class="breadcrumbs">','</div>' );
+            }
+        ?>
     </div>
+    <div class="grid-container margin-top-4">
+        <div class="grid-row grid-gap">
+            <?php
+            if($sidebar_position == 'left'):
+                benjamin_get_sidebar($template, $sidebar_position, $sidebar_size);
+            endif;
+            ?>
+            <div class="usa-prose <?php echo esc_attr($main_width); ?>">
+                <?php
+                    if (have_posts()) :
+                        /* Start the Loop */
+                        while (have_posts()) :
+                            the_post();
 
-    <?php
-    if ($sidebar_position == 'right') :
-        benjamin_get_sidebar($template, $sidebar_position, $sidebar_size);
-    endif;
-    ?>
+                        /*
+                        * Include the Post-Format-specific template for the content.
+                        * If you want to override this in a child theme, then include a file
+                        * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+                        */
+                            get_template_part('template-parts/feed/content', get_post_format());
+                        endwhile;
 
+                        benjamin_the_posts_navigation();
+                    else :
+                        get_template_part('template-parts/feed/content', 'none');
+                    endif;
+                ?>
+            </div>
+            <?php
+            if($sidebar_position == 'right'):
+                benjamin_get_sidebar($template, $sidebar_position, $sidebar_size);
+            endif;
+            ?>
+        </div>
+    </div>
 </section>
 
 <?php

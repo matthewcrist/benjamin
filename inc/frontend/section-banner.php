bd71e579d381bd0c50503c0988de4e6dad608556
Gabriel Ramirez <gabriel.e.ramirez@gmail.com>
2019-07-16 15:27:21 -0500

    Update notification banner color and navbar button colors

--- inc/frontend/section-banner.php
+++ inc/frontend/section-banner.php
@@ -4,7 +4,7 @@ $sidebars = wp_get_sidebars_widgets();
 
 ?>
 <!-- Gov banner BEGIN -->
-<header class="usa-dark-background padding-y-2">
+<header class="usa-light-background padding-y-2">
     <div class="grid-container text-white">
         <?php echo stripslashes(get_theme_mod('banner_text_setting', null));  //WPCS: xss ok. ?>
     </div>

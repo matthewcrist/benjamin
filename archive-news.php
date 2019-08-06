94e20e686bf9d1dd5f30cf03d400d03a05f30d50
Hondo <hi@hondo.xyz>
2019-08-05 21:39:36 -0400

    Adds single view of news

--- archive-news.php
+++ archive-news.php
@@ -44,27 +44,29 @@ if (!$hide_content) :
                 benjamin_get_sidebar($template, $sidebar_position, $sidebar_size);
             endif;
             ?>
-            <div class="usa-prose <?php echo esc_attr($main_width); ?>">
-                <h1>News &amp; Updates</h1>
-                <?php
-                    if (have_posts()) :
-                        /* Start the Loop */
-                        while (have_posts()) :
-                            the_post();
+            <div class="<?php echo esc_attr($main_width); ?>">
+                <h1 class="text-violet-70v font-sans-xl text-normal margin-top-0">News &amp; Updates</h1>
+                <div class="usa-prose">
+                    <?php
+                        if (have_posts()) :
+                            /* Start the Loop */
+                            while (have_posts()) :
+                                the_post();
 
-                        /*
-                        * Include the Post-Format-specific template for the content.
-                        * If you want to override this in a child theme, then include a file
-                        * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-                        */
-                            get_template_part('template-parts/feed/news', get_post_format());
-                        endwhile;
+                            /*
+                            * Include the Post-Format-specific template for the content.
+                            * If you want to override this in a child theme, then include a file
+                            * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+                            */
+                                get_template_part('template-parts/feed/news', get_post_format());
+                            endwhile;
 
-                        benjamin_the_posts_navigation();
-                    else :
-                        get_template_part('template-parts/feed/news', 'none');
-                    endif;
-                ?>
+                            benjamin_the_posts_navigation();
+                        else :
+                            get_template_part('template-parts/feed/news', 'none');
+                        endif;
+                    ?>
+                </div>
             </div>
             <?php
             if($sidebar_position == 'right'):

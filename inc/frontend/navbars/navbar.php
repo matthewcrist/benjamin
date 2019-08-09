c8bfd62f5bd4c10dfd3ca5f639dfa23e00570b6e
Hondo <hi@hondo.co>
2019-08-09 13:39:48 -0400

    Start on search

--- inc/frontend/navbars/navbar.php
+++ inc/frontend/navbars/navbar.php
@@ -12,23 +12,29 @@ $sticky = get_theme_mod('navbar_sticky_setting', 'no') == 'yes' ? 'sticky' : '';
       <button class="usa-menu-btn">Menu</button>
       <nav role="navigation" class="usa-nav display-flex flex-align-center">
         <button class="usa-nav__close"><img src="/assets/img/close.svg" alt="close"></button>
+        <form class="usa-search usa-search--small order-last width-card-lg">
+          <div role="search">
+            <label class="usa-sr-only" for="basic-search-field-small">Search small</label>
+            <input class="usa-input" id="basic-search-field-small" type="search" name="search">
+            <button class="usa-button" type="submit"><span class="usa-sr-only">Search</span></button>
+          </div>
+        </form>
         <?php
           $args =  array(
             'theme_location' => 'primary',
             'container' => '',
-            'menu_class' => 'usa-nav__primary usa-accordion',
+            'menu_class' => 'order-first usa-nav__primary usa-accordion',
             'walker' => new BenjaminNavbarWalker(),
             'fallback_cb' => 'benjamin_set_default_menu'
           );
 
           wp_nav_menu($args);
         ?>
-        <div></div>
-        <ul id="translate-navigation" class="usa-nav__primary usa-accordion notranslate">
+        <ul id="translate-navigation" class="usa-nav__primary usa-accordion notranslate" style="margin-top: 0;">
           <li class="usa-nav__primary-item">
             <button class="usa-accordion__button usa-nav__link usa-nav__translate" aria-expanded="false" aria-controls="translations">
               <?php include(get_template_directory() . '/assets/frontend/img/globe-americas.svg'); ?>
-              <span id="selectedLanguage">Translations</span>
+              <span id="selectedLanguage">Languages</span>
             </button>
             <ul class="usa-nav__submenu" id="translations" hidden="">
               <li class="usa-nav__submenu-item">
@@ -62,9 +68,6 @@ $sticky = get_theme_mod('navbar_sticky_setting', 'no') == 'yes' ? 'sticky' : '';
             </ul>
           </li>
         </ul>
-        <?php if (get_theme_mod('navbar_search_setting', 'none') == 'navbar') : ?>
-          <?php get_search_form(); ?>
-        <?php endif;?>
       </nav>
     </div>
   </div>
